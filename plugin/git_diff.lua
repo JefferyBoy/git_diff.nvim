57ddf896e4c88c755be6339c59107573fd8d7f90
jianfang.lei01 <jianfang.lei01@dreamsmart.com>
2025-01-24 11:00:24 +0800

    支持显示当前文件在其它分支的版本

--- plugin/git_diff.lua
+++ plugin/git_diff.lua
@@ -11,16 +11,19 @@ local function get_git_project_path()
 end
 
 -- 运行命令并在当前buffer的下面显示结果
-local function run_cmd_and_show_result(cmd)
+local function run_cmd_and_show_result(cmd, title)
 	local output = vim.fn.systemlist(cmd)
 	if #output > 0 then
 		-- 在当前buffer的下面拆分创建新的buffer
 		-- vim.api.nvim_command("botright new")
 		local file = vim.api.nvim_buf_get_name(0)
 		file = string.match(file, "([^/]+)$") or ""
+    if title == nil then
+      title = "Log:" .. file
+    end
 		local bufid = vim.api.nvim_create_buf(false, true)
 		vim.api.nvim_command("buffer " .. bufid)
-		vim.api.nvim_buf_set_name(bufid, "Log:" .. file)
+		vim.api.nvim_buf_set_name(bufid, title)
 		vim.api.nvim_buf_set_lines(bufid, 0, -1, false, output)
 		vim.api.nvim_buf_set_option(bufid, "buftype", "nofile")
 		-- vim.api.nvim_buf_set_option(0, "bufhidden", "delete")
@@ -37,6 +40,7 @@ end
 -- 显示当前文件的修改历史记录
 function M.show_git_file_history()
 	local file_path = vim.fn.expand("%:p")
+  local file_name = vim.fn.expand("%:t")
 	local repo_root = get_git_project_path()
 	if repo_root == "" or repo_root == "fatal: Not a git repository (or any of the parent directories): ." then
 		vim.api.nvim_echo(
@@ -48,12 +52,13 @@ function M.show_git_file_history()
 	end
 	file_path = string.gsub(file_path, repo_root, ".", 1)
 	local command = string.format('git -C "%s" log --patch "%s"', repo_root, file_path)
-	run_cmd_and_show_result(command)
+	run_cmd_and_show_result(command, "History:" .. file_name)
 end
 
 -- 显示当前文件的修改对比
 function M.show_git_file_diff()
 	local file_path = vim.fn.expand("%:p")
+  local file_name = vim.fn.expand("%:t")
 	local repo_root = get_git_project_path()
 	if repo_root == "" or repo_root == "fatal: Not a git repository (or any of the parent directories): ." then
 		vim.api.nvim_echo(
@@ -65,7 +70,7 @@ function M.show_git_file_diff()
 	end
 	file_path = string.gsub(file_path, repo_root, ".", 1)
 	local command = string.format('git -C "%s" diff "%s"', repo_root, file_path)
-	run_cmd_and_show_result(command)
+	run_cmd_and_show_result(command, "Diff:" .. file_name)
 end
 
 local function show_diff_by_branch_internal(bufid, winid)
@@ -77,9 +82,10 @@ local function show_diff_by_branch_internal(bufid, winid)
 		vim.api.nvim_echo({ { "Not in a Git repository", "WarningMsg" } }, true, {})
 		return
 	end
+  local file_name = vim.fn.expand("%:t")
 	local file_path = string.gsub(vim.fn.expand("%:p"), repo_root, ".", 1)
 	local cmd = string.format('git -C "%s" diff "%s" "%s"', repo_root, branch[1], file_path)
-	run_cmd_and_show_result(cmd)
+	run_cmd_and_show_result(cmd, "Diff:" .. file_name)
 end
 
 -- 对比另一个分支的相同文件
@@ -118,16 +124,52 @@ function M.show_diff_by_branch()
 	end, { silent = true, buffer = bufid })
 end
 
+function M.show_diff_by_branch2(args)
+  local branch = args.fargs[1]
+	local repo_root = get_git_project_path()
+	if repo_root == "" then
+		vim.api.nvim_echo({ { "Not in a Git repository", "WarningMsg" } }, true, {})
+		return
+	end
+  local file_name = vim.fn.expand("%:t")
+	local file_path = string.gsub(vim.fn.expand("%:p"), repo_root, ".", 1)
+	local cmd = string.format('git -C "%s" diff "%s" "%s"', repo_root, branch, file_path)
+	run_cmd_and_show_result(cmd, branch .. ":" .. file_name)
+end
+
+-- 查看指定分支的文件
+function M.show_file_by_branch(args)
+  local branch = args.fargs[1]
+	local file_path = vim.fn.expand("%:p")
+  local file_name = vim.fn.expand("%:t")
+	local repo_root = get_git_project_path()
+	if repo_root == "" or repo_root == "fatal: Not a git repository (or any of the parent directories): ." then
+		vim.api.nvim_echo(
+			{ { "Not in a Git repository or unable to find the repository root.", "WarningMsg" } },
+			true,
+			{}
+		)
+		return
+	end
+	file_path = string.gsub(file_path, repo_root, ".", 1)
+	local command = string.format('git -C "%s" show %s:"%s"', repo_root, branch, file_path)
+	run_cmd_and_show_result(command, branch .. ":" .. file_name)
+end
+
 vim.api.nvim_create_user_command("GitDiffFileHistory", M.show_git_file_history, {
 	nargs = 0,
-	desc = "Show git log for current file",
+	desc = "Show history of current file",
 })
 vim.api.nvim_create_user_command("GitDiffFileChanges", M.show_git_file_diff, {
 	nargs = 0,
-	desc = "Show git diff for current file",
+	desc = "Show changes of current file",
 })
-vim.api.nvim_create_user_command("GitDiffFileByBranch", M.show_diff_by_branch, {
-	nargs = 0,
-	desc = "Show git diff for current file with another branch",
+vim.api.nvim_create_user_command("GitDiffFileByBranch", M.show_diff_by_branch2, {
+	nargs = 1,
+	desc = "Show file diff of current file with another branch",
+})
+vim.api.nvim_create_user_command("GitShowFileByBranch", M.show_file_by_branch, {
+	nargs = 1,
+	desc = "Show current file in another branch",
 })
 return M
